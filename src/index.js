61606b8a57941c6be2946a6ab4c5290b831710d1
Guillermo Casal Caro <guillermo.casal.caro@gmail.com>
2024-01-15 16:41:50 +0100

    done

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import reactDom from 'react-dom';
-import App from './layout/App';
-
-if (module.hot) {
-    module.hot.accept();
-}
-
-reactDom.render(<App />, document.querySelector('#root'));
